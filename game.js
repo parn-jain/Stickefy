632426d53fcf26ce932707ea479b12bebf20950c
“parn” <pjisvgreat@gmail.com>
2022-09-22 20:40:48 +0530

    save till hear

---
+++ game.js
@@ -0,0 +1,46 @@
+let html = document.getElementsByClassName("test").innerHTML;
+console.log(html);
+// const a1 = new Audio(`songs/${html}.mp3`);
+const a1 = new Audio();
+function first(num) {
+    a1.src = `songs/${num}.mp3`;
+    var play = document.getElementsByClassName("fa");
+    console.log(play[num].classList.value.split(" ")[1])
+    if (play[num].classList.value.split(" ")[1] == "fa-play") {
+        for (let i = 0; i < play.length; i++) {
+                play[i].classList.replace("fa-pause", "fa-play");
+                // a1.pause();
+            }
+            play[num].classList.replace("fa-play", "fa-pause");
+                            a1.play();
+                        } 
+                        
+                        else
+                        {
+                            console.log("yes");
+                            for (let i = 0; i < play.length; i++) {
+                                play[i].classList.replace("fa-pause", "fa-play");
+                                // a1.pause();
+                            }
+                            play[num].classList.replace("fa-pause", "fa-play");
+                            a1.pause();
+                            console.log("this");
+
+        }
+
+        // a1.pause();
+
+        // if(a1.paused)
+        // {
+        //     console.log("play");
+        //     a1.play();
+        // }
+
+        // else
+        // {
+        //     console.log("pause")
+        //     a1.pause();
+        // }
+    }
+    
+    
