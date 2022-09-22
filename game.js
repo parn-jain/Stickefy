6590d4c3262df85489333489755d97734ae8bed7
“parn” <pjisvgreat@gmail.com>
2022-09-23 00:26:33 +0530

    minimilistic music player done, trying to make hover effect

--- game.js
+++ game.js
@@ -1,46 +1,30 @@
 let html = document.getElementById("test").innerHTML;
-console.log(html);
-const a1 = new Audio(`songs/${html}.mp3`);
-// const a1 = new Audio();
-// a1.src = `songs/${num}.mp3`;
+// var SongNo =0;
+// var css = document.getElementsByClassName("Sname")[SongNo].innerHTML;
+// console.log(html);
+const a1 = new Audio();
+let number = -1;
 function first(num) {
     var play = document.getElementsByClassName("fa");
     console.log(play[num].classList.value.split(" ")[1])
     if (play[num].classList.value.split(" ")[1] == "fa-play") {
         for (let i = 0; i < play.length; i++) {
-                play[i].classList.replace("fa-pause", "fa-play");
-                // a1.pause();
-            }
-            play[num].classList.replace("fa-play", "fa-pause");
-                            a1.play();
-                        } 
-                        
-                        else
-                        {
-                            console.log("yes");
-                            for (let i = 0; i < play.length; i++) {
-                                play[i].classList.replace("fa-pause", "fa-play");
-                                // a1.pause();
-                            }
-                            play[num].classList.replace("fa-pause", "fa-play");
-                            a1.pause();
-                            console.log("this");
-
+            play[i].classList.replace("fa-pause", "fa-play");
         }
-
-        // a1.pause();
-
-        // if(a1.paused)
-        // {
-        //     console.log("play");
-        //     a1.play();
-        // }
-
-        // else
-        // {
-        //     console.log("pause")
-        //     a1.pause();
-        // }
+        play[num].classList.replace("fa-play", "fa-pause");
+        if(number != num){
+            number = num
+            a1.src = `songs/${number}.mp3`
+        }
+        a1.play();
+        
+    } else {
+        for (let i = 0; i < play.length; i++) {
+            play[i].classList.replace("fa-pause", "fa-play");
+        }
+        play[num].classList.replace("fa-pause", "fa-play");
+        a1.pause();
     }
-    
-    
+}
+
+
