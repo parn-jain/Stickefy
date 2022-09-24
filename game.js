d456cf2b504b201dfed7159a37885829705b2a5f
“parn” <pjisvgreat@gmail.com>
2022-09-24 16:39:11 +0530

    Showing which song is paused

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 let html = document.getElementById("test").innerHTML;
-// var SongNo =0;
-// var css = document.getElementsByClassName("Sname")[SongNo].innerHTML;
-// console.log(html);
 const a1 = new Audio();
 let number = -1;
 var bars = document.getElementsByClassName("playing");
 function first(num) {
+// this part is working on ,  play song when clicked, get sequence number given to the argument of function fist, pause all the other 
+//sound by using a for loop,       replacing play class and pause class (only 1 i function is used fa-play and fa-pause change
+// hote jaa rahe hay)
     var play = document.getElementsByClassName("fa");
     console.log(play[num].classList.value.split(" ")[1])
     if (play[num].classList.value.split(" ")[1] == "fa-play") {
@@ -13,13 +13,14 @@ function first(num) {
             play[i].classList.replace("fa-pause", "fa-play");
         }
         play[num].classList.replace("fa-play", "fa-pause");
-        if(number != num){
+        if (number != num) {
             number = num
-            a1.src = `songs/${number}.mp3`
+            a1.src = `songs/${number}.mp3`;
+
         }
         a1.play();
-        
-        
+
+
     } else {
         for (let i = 0; i < play.length; i++) {
             play[i].classList.replace("fa-pause", "fa-play");
@@ -28,41 +29,41 @@ function first(num) {
         a1.pause();
     }
 
-    if(a1.played)
-        {
-            console.log("hhhhhhhhhh");
-            for (let j = 0; j < bars.length; j++)
-            {
-                bars[j].classList.remove("last");
-            }
-            bars[num].classList.add("last");
-            
-        }
-        if(a1.paused)
-        {
-            bars[num].classList.remove("last");
 
+// Playing Green bars of sound--------------------------------------------------------------------------------------------------
+    if (a1.played) {
+        console.log("hhhhhhhhhh");
+        for (let j = 0; j < bars.length; j++) {
+            bars[j].classList.remove("playingGIF");
         }
+        bars[num].classList.add("playingGIF");
+
+    }
+    if (a1.paused) {
+        bars[num].classList.remove("playingGIF");
+
+    }
 }
 
-
+//------------------------------------------------------------------------------------------------------------------------------
+// Play button visible on hover  and removed on un-hover ------------------------------------------------------------------------------------------------------
 var trying = document.getElementsByClassName("fa");
-var happy = document.getElementsByClassName("no");
-function hover(hoi)
-{
+var SerialNo = document.getElementsByClassName("no");
+function hover(hoi) {
     console.log("hello , i am working");
     trying[hoi].classList.add("fafake");
-    happy[hoi].classList.add("pfake")
-    
+    SerialNo[hoi].classList.add("pfake");
 }
 
-function leave(hat)
-{
-    console.log("good bye");
-    trying[hat].classList.remove("fafake");
-    happy[hat].classList.remove("pfake");
+function leave(hat) {
 
+    if(number!=hat)
+    {
 
+        trying[hat].classList.remove("fafake");
+        SerialNo[hat].classList.remove("pfake");
+    }   
 }
+// ------------------------------------------------------------------------------------------------------------------------------------
 
 
